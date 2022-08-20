e97aa083580025ae5a58f7a902a3a0ac36508abd
Haydz <hayden.mcleod@gmail.com>
2022-08-20 16:28:54 +1200

    refactor dates to use strings, fix and add tests

--- src/moments/core.clj
+++ src/moments/core.clj
@@ -2,18 +2,18 @@
   (:require [java-time :as jt])
   (:require [clojure.pprint :refer [pprint]]))
 
-(def moments 
-  (read-string (slurp "data/moments.edn")))
-
-(def todays-date (jt/local-date)) ;get todays date
-
 (defn time-between [todays-date moment-date]
-  (jt/time-between todays-date (jt/local-date moment-date) :days))
+  (jt/time-between (jt/local-date todays-date) (jt/local-date moment-date) :days))
 
 (defn enrich-moments [moments todays-date]
-  (map #(assoc % :duration-in-days (time-between todays-date (:date %))) moments))
+  (mapv #(assoc % :duration-in-days (time-between todays-date (:date %))) moments))
+
+(def moments
+  (read-string (slurp "data/moments.edn"))) ; not a pure function? how to structure where this goes?
+
+(def todays-date (str (jt/local-date))) ;get todays date. not a pure function? how to structure where this goes?
 
 (defn -main
-  []
+  [] 
   (pprint (enrich-moments moments todays-date)))
 
--- test/moments/core_test.clj
+++ test/moments/core_test.clj
@@ -2,13 +2,18 @@
   (:require [clojure.test :refer :all]
             [moments.core :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(deftest time-between-test 
+  (testing "Tests whether the difference of two dates are returned as a duration"
+    (is (= (moments.core/time-between "2022-09-01" "2022-08-01") -31))
+    (is (= (moments.core/time-between "2022-08-01" "2022-09-01") 31))
+    (is (= (moments.core/time-between "2022-09-01" "2022-09-01") 0))))
 
-(deftest positive-duration
-  (testing "When a moment occurs in the future, we should receive a positive number")
-  (def moments [{:title "Test Moment" :date "2022-09-01"}])
-  (def todays-date "2022-08-01")
-  (def duration (enrich-moments moments date))
-  (is (= duration 30)))
+(deftest enrich-moments-test
+  (testing "Tests whether moments data is enriched with calculated data such as the duration"
+    (let [moments-test-data
+         [{:title "Test Date 1" :date "2009-08-14"} 
+          {:title "Test Date 2" :date "2050-08-01"}]]
+    
+      (is (= (moments.core/enrich-moments moments-test-data "2022-01-01") 
+             [{:title "Test Date 1" :date "2009-08-14" :duration-in-days -4523}
+              {:title "Test Date 2" :date "2050-08-01" :duration-in-days 10439}])))))
