e75f8706c4c4c233352dfa61a7669f4d55562f31
Haydz <hayden.mcleod@gmail.com>
2022-08-20 16:47:49 +1200

    Setup test to ask Steve a question (PersistentList vs LocalDate)

--- test/moments/core_test.clj
+++ test/moments/core_test.clj
@@ -1,6 +1,7 @@
 (ns moments.core-test
   (:require [clojure.test :refer :all]
-            [moments.core :refer :all]))
+            [moments.core :refer :all])
+   (:require [java-time :as jt]))
 
 (deftest time-between-test 
   (testing "Tests whether the difference of two dates are returned as a duration"
@@ -17,3 +18,9 @@
       (is (= (moments.core/enrich-moments moments-test-data "2022-01-01") 
              [{:title "Test Date 1" :date "2009-08-14" :duration-in-days -4523}
               {:title "Test Date 2" :date "2050-08-01" :duration-in-days 10439}])))))
+
+(deftest test-to-show-steve 
+  (testing "Why does read-string return the data type PersistentList for the date compared to the original type of LocalDate?"
+    (is (= 
+         (type (:date (read-string (slurp "data/moments-test.edn")))) 
+         (type (:date {:title "Test Date" :date (jt/local-date 2009 8 14)}))))))
