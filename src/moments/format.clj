664d156a991fe24853fe304b595f9107beb1930c
Haydz <hayden.mcleod@gmail.com>
2022-08-24 12:09:59 +1200

    continue tinkering

--- src/moments/format.clj
+++ src/moments/format.clj
@@ -3,9 +3,15 @@
             [clojure.string :refer [trim]]))
 
 (defn add-tense [time-period formatted-time-period]
-  (if (.isNegative time-period)
-    (str formatted-time-period " ago")
-    (str "in " formatted-time-period)))
+  (cond
+    (= formatted-time-period "Today!") formatted-time-period
+    (.isNegative time-period) (str formatted-time-period " ago")
+    :else (str "in " formatted-time-period)))
+
+(defn today? [auto-format-duration]
+  (if (empty? auto-format-duration)
+    "Today!"
+    auto-format-duration))
 
 (defn get-units-of-time [time-period]
   {"year" (.getYears time-period)
@@ -14,7 +20,7 @@
 
 (defn auto-format 
   "Formats a date that will only include a time period (years, months and days) if the time period has a value"
-  [format units-of-time-for-period]
+  [format moment-period units-of-time-for-period]
   (->> (reduce (fn [accumulator unit-mapping]
             (let [absolute-duration (abs (second unit-mapping))
                   unit-of-time (first unit-mapping)]
@@ -24,12 +30,14 @@
                 :else (conj accumulator (str absolute-duration " " unit-of-time "s ")))))
           []
           units-of-time-for-period)
-      (apply str)
-      (trim)))
+       (apply str)
+       (trim)
+       (today?)
+       (add-tense moment-period)))
 
 (defn format-duration-dispatch [moment todays-date]
   (cond
-    (c/today? (:date moment) todays-date) :today
+    (c/today? (:date moment) todays-date) :auto-or-full;:today
     :else :auto-or-full))
 
 (defmulti format-duration format-duration-dispatch)
@@ -39,8 +47,7 @@
   ;(full-format (c/moment-period todays-date (:date moment))))
   (let [moment-period (c/moment-period todays-date (:date moment))]
     (->> (get-units-of-time moment-period)
-         (auto-format (:format moment))
-         (add-tense moment-period))))
+         (auto-format (:format moment) moment-period))))
 
 (defmethod format-duration :today
   [_moment _todays-date]
