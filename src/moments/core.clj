bd1868ff29d3ed68a3052f1ae017368f7804f30e
Haydz <hayden.mcleod@gmail.com>
2022-08-27 11:09:07 +1200

    play around with jetty

--- src/moments/core.clj
+++ src/moments/core.clj
@@ -6,13 +6,13 @@
 
 (defn load-moments-from-file [dir]
   (-> (slurp dir)
-  (read-string)))
+      (read-string)))
 
 (defn enrich-moments [moment todays-date]
   (assoc moment
-         :duration-in-days (c/time-between todays-date (:date moment)) 
+         :duration-date-to-today (c/time-between todays-date (:date moment))
          :period (jt/as-map (c/moment-period todays-date (:date moment)))
-         :formatted-date (jt/format "dd MMM YYYY" (jt/local-date(:date moment)))
+         :formatted-date (jt/format "dd MMM YYYY" (jt/local-date (:date moment)))
          :formatted-period (f/format-duration todays-date moment)))
 
 (defn process-moments [moments todays-date]
@@ -21,7 +21,7 @@
 (defn main'
   "pure driver for logic"
   []
-  (let [moments (load-moments-from-file "resources/moments.edn") 
+  (let [moments (load-moments-from-file "resources/moments.edn")
         todays-date (str (jt/local-date))]
     (process-moments moments todays-date)))
 
@@ -35,6 +35,10 @@
   (do (main') nil)
   (-main)
 
-  (jt/format "dd MMM YYYY" (jt/local-date "2022-01-01"))
+  #_#_(defn handler [request]
+        {:status 200
+         :headers {"Content-Type" "text/plain"}
+         :body "Hello Momenters!"})
+    (raj/run-jetty handler {:port 3000}) ; playing around with ring. requires [ring.adapter.jetty :as raj]
 
-  )
+  (jt/format "dd MMM YYYY" (jt/local-date "2022-01-01")))
